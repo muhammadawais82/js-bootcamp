3a1b7816f79263ad235fb727cf7e1d87b55ab3f0
muhammadawais82 <ffor971@gmail.com>
2020-03-25 12:35:31 +0500

    update number in js

--- basic.js
+++ basic.js
@@ -14,8 +14,17 @@ let isFreezing = temp <= 32
 if (isFreezing){
     console.log("It is freezing outside")
 }
+//////////////////////////////////////////////////////////////////
 
 // === is equal to
 // is !== not equal to
 // <= less then or equal to
 // >== greater then or equal to
+
+///////////////////////////////////////////////////////////////////
+//learn about number in javascript
+let obtain_marks = 50;
+let total_marks = 100;
+
+let percent = (obtain_marks / total_marks) * 100
+console.log(percent);
