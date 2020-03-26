a5f8f118043f1aadcaafb46667258e9935893521
muhammadawais82 <ffor971@gmail.com>
2020-03-26 09:12:39 +0500

    new arrays examples of uc

---
+++ array-uc.js
@@ -0,0 +1,6 @@
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+var hasEnoughPlayers = function(team){
+    return team.length == 7;
+}
+console.log(hasEnoughPlayers(team));
+
