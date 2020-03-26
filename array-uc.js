a1aa278ac62d330174c33f6ab45f31f439e96946
muhammadawais82 <ffor971@gmail.com>
2020-03-26 09:17:14 +0500

    add map mehtod and example

--- array-uc.js
+++ array-uc.js
@@ -43,3 +43,29 @@ words.forEach(function(word, num, all) {
 });
 
 /////////////////////////////////////////
+
+///////////////////////////////////////////
+//map method
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+var improvedDonuts = donuts.map(function (donut) {
+    donut += " hole";
+    donut = donut.toUpperCase();
+    return donut;
+});
+
+
+console.log(improvedDonuts)
+
+///////////////////////////////
+var bills = [50.23, 19.12, 34.01,
+    100.11, 12.15, 9.90, 29.11, 12.99,
+    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+
+var newBills = bills.map(function(bill){
+    bill += 0.15;
+    return bill;
+});
+console.log(newBills);
+
