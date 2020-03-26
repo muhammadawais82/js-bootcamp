db22669d0a8c70f122d0c0c1939c3e328b61de4e
muhammadawais82 <ffor971@gmail.com>
2020-03-26 09:14:02 +0500

    update and add push method

--- array-uc.js
+++ array-uc.js
@@ -4,3 +4,24 @@ var hasEnoughPlayers = function(team){
 }
 console.log(hasEnoughPlayers(team));
 
+////////////////////////////////
+//push method
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+var doctor = "Simon";
+var sister = "River";
+var shepherd = "Book";
+
+crew.push(doctor);
+crew.push(sister);
+crew.push(shepherd);
+console.log(crew);
+
+//////////////////////////////////
